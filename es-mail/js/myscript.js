4e1d5231ec5cdc717f92171ddbcb25daa8d5e667
Vale <oirelav95@gmail.com>
2022-11-11 13:13:39 +0100

    aggiunto evento al click del bottone

--- es-mail/js/myscript.js
+++ es-mail/js/myscript.js
@@ -3,31 +3,40 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.*/
 
-// Chiedo all'utente di inserire la sua email
-const emailUtente = prompt("Inserisci la tua email");
+// Variabile per l'input
+let emailUtente = document.getElementById("email-utente");
 
-console.log("Email Utente:", emailUtente);
+// Variabile per il bottone
+const myButton = document.getElementById("verifica");
+
+console.log("Email Utente:", emailUtente.value);
 
 // Creo array con le email
 const emailInvitati = ["pippo@gmail.com", "pluto@gmail.com", "vale@yahoo.it", "ciaobello@tiscali.it"];
 
-// Messaggio che appare se l'utente non può accedere
-let messaggio = "Non puoi accedere";
-
 // Calcolo il numero di elementi dell'array
 const numInvitati = emailInvitati.length;
 
-// Scorro tutto l'array
-for ( let i = 0; i < numInvitati; i++) {
-    console.log(`emailInvitati[${i}]`, emailInvitati[i]);
+myButton.addEventListener("click", 
 
-    // Se l'email inserita dall'utente coincide con una presente nell'array
-    if (emailUtente === emailInvitati[i]) {
-        // Messaggio che appare se l'utente può accedere
-        messaggio = "Benvenuto";  
+    function() {
+
+        // Messaggio che appare se l'utente non può accedere
+        let messaggio = "Non puoi entrare";
+
+        // Scorro tutto l'array
+        for ( let i = 0; i < numInvitati; i++) {
+            console.log(`emailInvitati[${i}]`, emailInvitati[i]);
+
+            // Se l'email inserita dall'utente coincide con una presente nell'array
+            if (emailUtente.value === emailInvitati[i]) {
+                // Messaggio che appare se l'utente può accedere
+                messaggio = "Benvenuto";  
+            }
+
+            // Assegno il messaggio all'id nell'html
+            document.getElementById("messaggio").innerHTML = messaggio;
+
+        }
     }
-
-    // Assegno il messaggio all'id nell'html
-    document.getElementById("messaggio").innerHTML = messaggio;
-
-}
+);
